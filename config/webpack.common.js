141b3d90da17bb10866cfd0a29e7b05a05e05777
Artem <N.Artem.work@yandex.ru>
2021-01-06 22:41:20 +0300

    add react

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -45,7 +45,14 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: ['babel-loader'],
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-react'],
+            },
+          },
+        ],
       },
       {
         test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
